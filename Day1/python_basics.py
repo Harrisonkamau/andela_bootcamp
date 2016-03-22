feb31278d3b5dae4256592921dfb7d2609dc2270
Harrisonkamau <kamauharrison@87gmail.com>
2016-03-22 08:41:50 +0300

    initial commit

---
+++ Day1/python_basics.py
@@ -0,0 +1,11 @@
+# Python has several built-in data structures.
+# They include: 1. Lists
+#               2. Tuples
+#               3. Dictionaries
+#               4. Sets
+# Several operations, known as methods, can be performed on these data structures
+
+# 1. Lists
+my_list = [1,2, True, 23.32, 'Hello','World']
+my_list.append(23)
+print(my_list)
