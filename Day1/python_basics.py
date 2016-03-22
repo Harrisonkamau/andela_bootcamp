84ae7dcc11608756a9b308df1df5f46835bd8f24
Harrisonkamau <kamauharrison@87gmail.com>
2016-03-22 11:28:25 +0300

    Added a text file with my name in it

--- Day1/python_basics.py
+++ Day1/python_basics.py
@@ -9,4 +9,18 @@
 my_list = [1,2, True, 23.32, 'Hello','World']
 my_list.append(23)
 print(my_list)
+# lists are mutable s
+
+# 2. Dictionaries
+empty_dict = dict()
+new_dict = {}
 my_profile = {"github": "Harrisonkamau", "company": "Andela", "age": 24}
+# Dictionaries are mutable and hence their values can be modified
+
+# 3. Tuples
+# One major attribute of a tuple is immutability
+
+my_tup = (12, 24.1, True, "Hello")
+
+# 4. Sets
+my_set = {1,2,3,True, 32.32}
